0ee836e04568e21a462f06e91f4121b60b56e273
Azharuddin <akhan@gainsight.com>
2017-10-29 03:23:01 +0530

    Formatted the code

--- index.go
+++ index.go
@@ -1,19 +1,20 @@
-package main 
+package main
 
 import (
-	"io/ioutil"
-	"fmt"
-	"net/http"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 var respString string = ""
+
 const url string = "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=87295cd4d5d4415c9cb4896590ccf2c3"
 
 func handleRoot(resp http.ResponseWriter, req *http.Request) {
-	
+
 	fmt.Fprintf(resp, respString)
-} 
+}
 
 func processString(item newsItem) string {
 	return fmt.Sprintf(`<div>
@@ -22,29 +23,29 @@ func processString(item newsItem) string {
 }
 
 type news struct {
-	Status string `json:"status"`;
-	Source string `json:"source"`;
-	SortBy string `json:"sortBy"`;
-	Articles []newsItem `json:"articles"`;
+	Status   string     `json:"status"`
+	Source   string     `json:"source"`
+	SortBy   string     `json:"sortBy"`
+	Articles []newsItem `json:"articles"`
 }
 
 type newsItem struct {
-	Author string `json:author`;
-	Title string `json:title`;
-	Description string `json:description`;
-	Url string `json:url`;
-	UrlToImage string `json:urlToImage`;
-	PublishedAt string `json:publishedAt`;
+	Author      string `json:author`
+	Title       string `json:title`
+	Description string `json:description`
+	Url         string `json:url`
+	UrlToImage  string `json:urlToImage`
+	PublishedAt string `json:publishedAt`
 }
 
-func main(){
+func main() {
 	var newsResults news
 	resp, _ := http.Get(url)
 	bytes, _ := ioutil.ReadAll(resp.Body)
 
 	err := json.Unmarshal(bytes, &newsResults)
 
-	if(err != nil) {
+	if err != nil {
 		fmt.Println("We have an error")
 		return
 	}
@@ -57,6 +58,5 @@ func main(){
 	fmt.Println("Server has started, browse http://localhost:9080 to check out news")
 	http.HandleFunc("/", handleRoot)
 	http.ListenAndServe(":9080", nil)
-	
-}
 
+}
